ad2adf87043b0366b45345849097dcedab674727
Jawad Rizvi <jawadrizvi40@gmail.com>
2020-01-19 21:26:51 -0700

    Updates

---
+++ precache-manifest.9185a1b080b32b1043fc642c6a264ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f72d6f1a46a9499716bd8032e116c95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bdf81b5a4cb708eed7d9",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da8071be5573416a80ca",
+    "url": "./static/js/2.2ee7131c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./static/js/2.2ee7131c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdf81b5a4cb708eed7d9",
+    "url": "./static/js/main.72899f03.chunk.js"
+  },
+  {
+    "revision": "1b6e6a7966e1ba2b39b9",
+    "url": "./static/js/runtime-main.8346aaca.js"
+  }
+]);
